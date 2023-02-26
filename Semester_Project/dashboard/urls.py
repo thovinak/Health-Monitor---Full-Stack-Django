4a5e868ba4c873e4c2a6ac56f91ced80c1c14c97
Karthik Thovinakere <thovinak@mcmaster.ca>
2023-02-26 14:57:39 -0500

    Added ecg signal
    Added my ecg signal from assignment 1 to portfolio

--- Semester_Project/dashboard/urls.py
+++ Semester_Project/dashboard/urls.py
@@ -6,4 +6,5 @@ app_name = 'dashboard'
 urlpatterns = [
     path('', views.index, name='index'),
     path("sinewave", views.sinewave, name='sinewave'),
+    path("ecgsignal", views.ecgsignal, name='ecgsignal'),
 ]
