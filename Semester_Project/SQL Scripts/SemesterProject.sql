90c09b93f3b240fb078aaf428087acc3b2416c06
Karthik Thovinakere <thovinak@mcmaster.ca>
2023-04-04 17:24:14 -0400

    Week 8 Wed- SQL init
    -Changed docker-compose to include redis
    - added sql

---
+++ Semester_Project/SQL Scripts/SemesterProject.sql
@@ -0,0 +1,2 @@
+create schema if not exists SemesterProject;
+grant all on SemesterProject.* to learningdjango;
--- Semester_Project/dashboard/models.py
+++ Semester_Project/dashboard/models.py
@@ -1,7 +1,16 @@
 from django.db import models
+import datetime
 
 
-# Create your models here.
 class SineData(models.Model):
     value = models.DecimalField(decimal_places=32, max_digits=32)
     label = models.TimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-label']
+
+    def get_data(self, start, end):
+        self.start = datetime.time(start)
+        self.end = datetime.time(end)
+        self.data = SineData.objects.filter(label__gte=self.start, label__lte=self.end)
+        return self.data
